31a7e0f07976a2433cae949844c18bbc81d99df4
Deo <lukyanenko88@gmail.com>
2021-01-11 21:51:12 +0200

    Initial commit

---
+++ src/test/java/com/deo/meater/MeaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deo.meater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
