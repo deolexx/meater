9703b31d7f36aa578135aceb323d73c8db57347d
Deo <lukyanenko88@gmail.com>
2021-01-12 16:07:05 +0200

    change thymeleaf to mustache

--- src/main/java/com/deo/meater/controller/GreetingsController.java
+++ src/main/java/com/deo/meater/controller/GreetingsController.java
@@ -3,15 +3,26 @@ package com.deo.meater.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class GreetingsController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="Bridgeboy") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Bridgeboy") String name, Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
+    }
+
+
+    @GetMapping()
+      public String main(Map<String, Object> model)  {
+
+        model.put("some","Hello Deo");
+        return "main";
     }
 
 }
