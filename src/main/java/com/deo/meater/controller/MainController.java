69c9977d9b5c7de101191b73d87e440372687158
deolexx <lukyanenko88@gmail.com>
2021-01-17 16:15:21 +0200

    db support import in progress replace mustache with freemarker(not tested)

--- src/main/java/com/deo/meater/controller/MainController.java
+++ src/main/java/com/deo/meater/controller/MainController.java
@@ -30,7 +30,7 @@ public class MainController {
 
 
     @GetMapping("/main")
-      public String main(@RequestParam(required = false)String filter, Model model)  {
+      public String main(@RequestParam(required = false, defaultValue = "")String filter, Model model)  {
 
         Iterable<Message> messages = messageRepository.findAll();
 
