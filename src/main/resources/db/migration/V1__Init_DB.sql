fb608fef704962ff3200b8f10db71d343c579d19
Deo <lukyanenko88@gmail.com>
2021-01-22 19:32:56 +0200

    change thymeleaf to mustache add db support add some bootstrap css to main page add authentication and user roles tested add file upload support add bootstrap support add menu to the app add some bootstrap stiles add delete button and date/time to messages db migration in progress connection error

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,4 +1,4 @@
-create sequence hibernate_sequence start 1 increment 1
+create sequence hibernate_sequence start 1 increment 1;
 
 create table message (
     id int8 not null,
@@ -8,12 +8,12 @@ create table message (
     text varchar(2048) not null,
     user_id int8,
     primary key (id)
-                     )
+                     );
 
 create table user_role (
     user_id int8 not null,
     roles varchar(255)
-                       )
+                       );
 
 create table usr (
     id int8 not null,
@@ -22,8 +22,8 @@ create table usr (
     email varchar(255),
     password varchar(255) not null,
     username varchar(255) not null,
-    primary key (id))
+    primary key (id));
 
-alter table message add constraint message_user_FK foreign key (user_id) references usr
+alter table message add constraint message_user_FK foreign key (user_id) references usr;
 
-alter table user_role add constraint user_role_user_FK foreign key (user_id) references usr
+alter table user_role add constraint user_role_user_FK foreign key (user_id) references usr;
