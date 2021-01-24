deaded936578fa4025771c5808cc8c5a813b46bb
Deo <lukyanenko88@gmail.com>
2021-01-24 21:28:02 +0200

    add profile page

--- src/main/java/com/deo/meater/controller/UserController.java
+++ src/main/java/com/deo/meater/controller/UserController.java
@@ -2,32 +2,30 @@ package com.deo.meater.controller;
 
 import com.deo.meater.entity.Role;
 import com.deo.meater.entity.User;
-import com.deo.meater.repos.UserRepo;
+import com.deo.meater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
-@PreAuthorize("hasAuthority('ADMIN')")
 @RequestMapping("/user")
 public class UserController {
     @Autowired
-    UserRepo userRepo;
+    UserService userService;
 
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
     @GetMapping("{user}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
@@ -35,25 +33,32 @@ public class UserController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user) {
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
-
-       user.getRoles().clear();
-
-       for(String key : form.keySet()){
-          if(roles.contains(key)){
-              user.getRoles().add(Role.valueOf(key));
-          }
-       }
-
-        userRepo.save(user);
+      userService.saveUser(user, username, form);
 
         return "redirect:/user";
+    }
+
+
+
+    @GetMapping("profile")
+    public String userProfile(Model model, @AuthenticationPrincipal User user){
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+      return "profile";
+    }
+    @PostMapping("profile")
+    public String updateProfile(@AuthenticationPrincipal User user,
+                                @RequestParam String password,
+                                @RequestParam String email){
+
+
+        userService.updateProfile(user,password,email);
+        return "redirect:/user/profile";
     }
 
 }
--- src/main/java/com/deo/meater/service/UserService.java
+++ src/main/java/com/deo/meater/service/UserService.java
@@ -10,17 +10,17 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
-    private  UserRepo userRepo;
+    private UserRepo userRepo;
 
     @Autowired
-     private MailSender mailService;
+    private MailSender mailService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -28,10 +28,9 @@ public class UserService implements UserDetailsService {
     }
 
 
-
-    public boolean addUser(User  user){
-        User userFromDb =   userRepo.findByUsername(user.getUsername());
-        if(userFromDb!=null){
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if (userFromDb != null) {
             return false;
         }
 
@@ -41,26 +40,76 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(UUID.randomUUID().toString());
 
 
-
         userRepo.save(user);
 
-        if(!StringUtils.isEmpty(user.getEmail()))
-        {
-            String message = String.format("Hello, %s! \n"+"Welcome to Meater. Please, visit next link: http://localhost:8080/activate/%s",user.getUsername(),user.getActivationCode());
-mailService.send(user.getEmail(),"Activation code",message);
-
-
-        }
+        sendMessage(user);
         return true;
     }
 
+    private void sendMessage(User user) {
+        if (!StringUtils.isEmpty(user.getEmail())) {
+             String message = String.format("Hello, %s! \n" + "Welcome to Meater. Please, visit next link: http://localhost:8080/activate/%s",
+                     user.getUsername(),
+                     user.getActivationCode());
+             mailService.send(user.getEmail(), "Activation code", message);
+
+
+         }
+    }
+
     public boolean activateUser(String code) {
-      User user =   userRepo.findByActivationCode(code);
-      if(user==null){
-          return false;
-      }
-      user.setActivationCode(null);
-      userRepo.save(user);
-        return  true;
+        User user = userRepo.findByActivationCode(code);
+        if (user == null) {
+            return false;
+        }
+        user.setActivationCode(null);
+        userRepo.save(user);
+        return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+
+            if (!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepo.save(user);
+
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
